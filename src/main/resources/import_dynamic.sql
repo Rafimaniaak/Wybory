de48b09989ecfe68bff6c5e929622b8ea6c36ab2
Rafimaniaak <rafimaniaak@gmail.com>
2025-06-03 22:48:39 +0200

    V0.8

--- src/main/resources/import_dynamic.sql
+++ src/main/resources/import_dynamic.sql
@@ -1,2 +1,4 @@
 INSERT INTO USERS (username, password, role, has_voted, first_name, last_name, pesel) VALUES ('user2', '$2a$10$SQoysXqwc4PRM69B5dREiu0KyK7mSlY8Jj65Dg7jWeVqnnqaNsLt.', 'USER', false, 'Paweł', 'Konopski', '90010254123');
 INSERT INTO USERS (username, password, role, has_voted, first_name, last_name, pesel) VALUES ('user3', '$2a$10$.GqJ9nycIrQIU8cWR3ygEe1JNsPNZ/M1ksruFfDRzB1Qa5G5G0tbm', 'USER', false, 'Alicja', 'Koralik', '90044254321');
+INSERT INTO USERS (id, username, password, role, has_voted) VALUES (2, 'user12345', '$2a$10$SvcT2pdsn5ndUfkfSvtFZ.dlYk/jnKfOz/gyq0yQdl58miFuAi23e', 'USER', false);
+INSERT INTO USERS (id, username, password, role, has_voted) VALUES (2, 'user', '$2a$10$SvcT2pdsn5ndUfkfSvtFZ.dlYk/jnKfOz/gyq0yQdl58miFuAi23e', 'USER', false);
